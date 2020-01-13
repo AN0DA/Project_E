03f3a820e1eda1a0050ca61b04758ad194044bef
M_Chlebosz <chleboszmateusz@gmail.com>
2020-01-13 12:43:28 +0100

    Modified: Multiply and divide

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-print("Podaj liczby do dodania:")
+print("Podaj liczby do mnożenia:")
 
 x = input("x = ")
 y = input("y = ")
 
-print(x+y)
+print(x*y)
+
+print("Podaj liczby do odejmowania:")
+
+x = input("x = ")
+y = input("y = ")
+
+print(x-y)
+

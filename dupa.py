1c18e5e4b0b2221d38b9f4c3bb5d35deaf57d978
Nemryk <nemrykpl@gmail.com>
2020-01-14 09:03:26 +0100

    Create dupa.py

---
+++ dupa.py
@@ -0,0 +1,14 @@
+print("Podaj liczby do mnożenia:")
+
+x = input("x = ")
+y = input("y = ")
+
+print(x*y)
+
+print("Podaj liczby do odejmowania:")
+
+x = input("x = ")
+y = input("y = ")
+
+print(x-y)
+

40cbe1fece25a3e0de1c3e032d1cd39ab1108fa9
Nemryk <nemrykpl@gmail.com>
2020-01-20 12:19:54 +0100

    Sranie
    sranie
    
    Co-Authored-By: Mikołaj Kaczmarek <mikson.kaczmarek@outlook.com>

--- Source/ProjectE/Public/world_generator.h
+++ Source/ProjectE/Public/world_generator.h
@@ -4,18 +4,26 @@
 #include "biome.h"
 #include "fragment.h"
 #include "CoreMinimal.h"
+#include "Engine/World.h"
 #include "GameFramework/Actor.h"
 #include "world_generator.generated.h"
 UCLASS()
 class PROJECTE_API Aworld_generator : public AActor
 {
 	GENERATED_BODY()
+
 public:	
 	Aworld_generator();
 	UPROPERTY(EditAnywhere, Category = "Water")
 	int water_level = 0;
 	UPROPERTY(EditAnywhere, Category = "Biomes")
 	TArray<Abiome*> biomes;
+	UPROPERTY(EditAnywhere, Category = "Core")
+		AActor* core;
+	UPROPERTY(EditAnywhere, Category = "Dynamic")
+		int dupka;
+	TArray<Afragment*> frag_;
+	
 	UPROPERTY(EditAnywhere, Category = "World Size")
 		int horizontal = 10, vertical = 10, number_of_biomes=1;
 		
@@ -27,18 +35,16 @@ public:
 public:
 	void Generate_common()
 	{
-		UWorld* w = GetWorld();
 		float temp_hor = 0, temp_ver = 0, temp_high_dif = biomes[0]->heigth_diffrences;
 		for (int i = 10;i <= horizontal;i++)
 		{
 			for (int j = 10;j <= vertical;j++)
 			{
-				
+
 				float temp_num = FMath::FRandRange(0, temp_high_dif);
-				FRotator rot = FRotator(0,0,0);
+				FRotator rot = FRotator(0, 0, 0);
 				FVector vec = FVector(temp_hor, temp_ver, temp_num);
-				w->SpawnActor<Afragment>(biomes[0]->fragment, rot, vec);
-				
+				GetWorld() -> SpawnActor<Afragment>(biomes[0]->fragment, rot, vec);
 				temp_ver += 125;
 			}
 			temp_hor += 125;

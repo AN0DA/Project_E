3d04339685602262b4c9a41e4e82bd2d16604986
asprtam <44498326+asprtam@users.noreply.github.com>
2020-05-08 12:12:55 +0200

    treemodel (#66)
    Co-authored-by: Mikołaj Kaczmarek <mikolaj.kaczmarek@uczen.zsk.poznan.pl>

--- Core.h
+++ Core.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include "world_gen.h"
--- field.h
+++ field.h
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+
 struct Field
 {
 	sf::Sprite sprite;

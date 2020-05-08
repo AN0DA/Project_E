a5a5ed63ab94e4d9a15ac5fb746f3bceb5ac6879
asprtam <44498326+asprtam@users.noreply.github.com>
2020-05-08 11:58:49 +0200

    lifeCycle
    lifeCycle and drinkWater added

--- Core.h
+++ Core.h
@@ -5,7 +5,6 @@
 
 #include "world_gen.h"
 
-
 class Core
 {
     
--- treeClass.h
+++ treeClass.h
@@ -8,12 +8,15 @@ class Tree {
 	std::vector<Field* > roots; // possible rectreate later, I dont know how do exacly squares of our world will work
 	std::vector<Field* > neighborFields; // possible rectreate later, I dont know how do exacly squares of our world will work
 	int currentWater;
+	int drainPerRoot;
 	int humidityUsage;
+	int waterToGrowth;
 	// to add later: health, 
 public:
 	Tree(Field* trunk);
-	void treeLifeCycle(int currentWater); // this function will need ticks as argument later
+	void treeLifeCycle(int interval); // this function will need ticks as argument later
 	void treeGrow();
+	void drinkWater();
 	void treeShrink();
 	void assignNewNeighbor(Field* newNeighbor);
 };

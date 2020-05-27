f508ceea2de4aba1b191c34f932782548bd1519c
Konstanty Kordas <kostekkor@gmail.com>
2020-05-27 22:37:41 +0200

    comments

--- env_humidity.cpp
+++ env_humidity.cpp
@@ -1,16 +1,38 @@
+/// \file
+/// \brief Source file for env_humidity
+/// \details Source file for env_humidity environment parameter. Generates river, lakes and humidity around them.
+/// \author Konstanty Kordas
+/// \date 27.05.2020
+/// \version 0.1
+
 #include "env_humidity.h"
 #include <cmath>
 
+
+/// \brief Script creating water tile
+/// \author Konstanty Kordas
+/// \date 27.05.2020
+/// \version 0.1
+///
+/// function creates sets water and calls circle_humidity() to generate humidity
 void env_humidity::create_water_tile(sprite_params** data, int width, int height, int coordX, int coordY, int humidity_reach) {
 	if (!data[coordX][coordY].isWater()) {
 		data[coordX][coordY].setWater(true);
 		circle_humidity(data, width, height, coordX, coordY, humidity_reach);
 	}
+	// debug print if a tile would be generated twice
 	else {
-		std::cout << coordX << "    " << coordY << std::endl;
+		//std::cout << coordX << "    " << coordY << std::endl;
 	}
 }
 
+
+/// \brief Script generating desert tiles
+/// \author Konstanty Kordas
+/// \date 27.05.2020
+/// \version 0.1
+///
+/// function generates random amunt of desert tiles in each row, at minimum width/3
 void env_humidity::generate_desert(sprite_params** data, int width, int height, int coordX, int coordY) {
 	for (int i = coordX; i < height; i++) {
 		int left = rand() % (width / 3);
@@ -21,6 +43,13 @@ void env_humidity::generate_desert(sprite_params** data, int width, int height, 
 	}
 }
 
+
+/// \brief Script creating lakes
+/// \author Konstanty Kordas
+/// \date 27.05.2020
+/// \version 0.1
+///
+/// function creates circular lakes by checking if the distance beetween a tile and the center of a lake is smaller than the radius, if yes then it creates a water tile there
 void env_humidity::generate_lake(sprite_params** data,int width,int height, int coordX, int coordY, int radius) {
 	for (int i = std::max((coordX - radius), 0); i <= std::min((coordX + radius), width); i++) {
 		for (int j = std::max((coordY - radius), 0); j <= std::min((coordY + radius), height); j++) {
@@ -30,6 +59,14 @@ void env_humidity::generate_lake(sprite_params** data,int width,int height, int 
 		}
 	}
 }
+
+
+/// \brief Script adding humidity
+/// \author Konstanty Kordas
+/// \date 27.05.2020
+/// \version 0.1
+///
+/// function adds humidity value in a circle by checking if the distance beetween a tile and the center of a lake is smaller than the radius, if yes then it creates a water tile there
 void env_humidity::circle_humidity(sprite_params** data, int width, int height, int coordX, int coordY, int radius) {
 	double temp_hum;
 	for (int i = std::max((coordX - radius),0); i <= std::min((coordX + radius),width); i++) {
@@ -41,18 +78,32 @@ void env_humidity::circle_humidity(sprite_params** data, int width, int height, 
 		}
 	}
 }
+
+
+
+/// \brief Script creating a river 
+/// \author Konstanty Kordas
+/// \date 27.05.2020
+/// \version 0.1
+///
+/// function generates a river
 void env_humidity::generate_river(sprite_params** data, int width, int height, int coordX, int coordY, int river_width,int river_turn) {
-	int turn_length = (river_width * 2) + (std::rand() % (width / 2 - (river_width * 4)));
-	bool side = coordX > (width / 2); //true=lewo,false=prawo
+	int turn_length = (river_width * 2) + (std::rand() % (width / 2 - (river_width * 4)));	//length of the sideways running river,generated so it will always fit inside the grid
+	bool side = coordX > (width / 2);	//true=left,false=right
 
 if (side) {
-	//lewo
+	//turn goes left
+
+
+	//river going from beginning to the turn
 	for (int j = coordY; j < river_turn; j++) {
 		for (int i = coordX - river_width; i <= coordX; i++) {
 			create_water_tile(data, width, height, i, j, std::max((2 + river_width) * 2, 7));
 		}
 	}
-	//zakrêt góra-lewo
+
+
+	//top-left turn
 	for (int j = river_turn; j <= river_turn + river_width; j++) {
 		for (int i = coordX - river_width; i <= coordX; i++) {
 			if (sqrt(((coordX - river_width - i) * (coordX - river_width - i)) + ((river_turn - j) * (river_turn - j))) <= river_width) {
@@ -60,13 +111,17 @@ if (side) {
 			}
 		}
 	}
-	//rzeka
+
+
+	//sideways river
 	for (int j = river_turn; j <= river_turn + river_width; j++) {
 		for (int i = coordX - river_width - turn_length; i < coordX - river_width; i++) {
 			create_water_tile(data, width, height, i, j, std::max((2 + river_width) * 2,7));
 		}
 	}
-	//zakrêt prawo-dó³
+
+
+	//right-down turn
 	for (int j = river_turn; j <= river_turn + river_width; j++) {
 		for (int i = coordX - river_width - turn_length - river_width; i < coordX - river_width - turn_length; i++) {
 			if (sqrt(((coordX - river_width - turn_length - i) * (coordX - river_width - turn_length - i)) + ((river_turn + river_width - j) * (river_turn + river_width - j))) <= river_width) {
@@ -74,7 +129,9 @@ if (side) {
 			}
 		}
 	}
-	//rzeka
+
+
+	//river going down from turn to the end of the grid
 	for (int j = river_turn + river_width+1; j < height; j++) {
 		for (int i = coordX - river_width - turn_length - river_width; i <= coordX - river_width - turn_length; i++) {
 			create_water_tile(data, width, height, i, j, std::max((2 + river_width) * 2,7));
@@ -82,16 +139,18 @@ if (side) {
 	}
 }
 else {
-	//prawo
+	//turn goes right
 
-	//rzeka
+
+	//river going from beginning to the turn
 	for (int j = coordY; j < river_turn; j++) {
 		for (int i = coordX; i <= coordX + river_width; i++) {
 			create_water_tile(data, width, height, i, j, std::max((2 + river_width) * 2,7));
 		}
 	}
 
-	//zakrêt góra-prawo
+
+	//top-right turn
 	for (int j = river_turn; j <= river_turn + river_width; j++) {
 		for (int i = coordX; i < coordX + river_width; i++) {
 			if (sqrt(((coordX + river_width - i) * (coordX + river_width - i)) + ((river_turn - j) * (river_turn - j))) <= river_width) {
@@ -100,14 +159,16 @@ else {
 		}
 	}
 
-	//rzeka
+
+	//sideways river
 	for (int j = river_turn; j <= river_turn + river_width; j++) {
 		for (int i = coordX + river_width; i < coordX + river_width + turn_length; i++) {
 			create_water_tile(data, width, height, i, j, std::max((2 + river_width) * 2,7));
 		}
 	}
 
-	//zakrêt lewo-dó³
+
+	//left-down turn
 	for (int j = river_turn; j < river_turn + river_width; j++) {
 		for (int i = coordX + river_width + turn_length; i <= coordX + river_width + turn_length + river_width; i++) {
 			if (sqrt(((coordX + river_width + turn_length - i) * (coordX + river_width + turn_length - i)) + ((river_turn + river_width - j) * (river_turn + river_width - j))) <= river_width) {
@@ -116,7 +177,8 @@ else {
 		}
 	}
 
-	//rzeka
+
+	//river going down from turn to the end of the grid
 	for (int j = river_turn + river_width; j <= height; j++) {
 		for (int i = coordX + river_width + turn_length; i <= coordX + +river_width + turn_length + river_width; i++) {
 			create_water_tile(data, width, height, i, j, std::max((2 + river_width) * 2,7));
@@ -125,128 +187,35 @@ else {
 }
 }
 void env_humidity::generate_humidity(sprite_params** data, int width, int height) {
-	//rzeka
+	//river asssociated generation
 
-	int river_top_start  = 33;// 1 + (std::rand() % (width - 1));
-	int river_width = rand() % (std::min(height, width) / 20);
-	int river_turn = (height / 10) + (std::rand() % (height - (height / 10)));
+	int river_top_start  = 1 + (std::rand() % (width - 1));  //beginnig point of a river
+	int river_width = rand() % (std::min(height, width) / 20);  //additional width of a river (actual river width = river_width + 1)
+	int river_turn = (height / 10) + (std::rand() % (height - (height / 10)));	//Y axis (height) index of the turn of the river
 	generate_river(data, width, height, river_top_start, 0, river_width,river_turn);
 
-	//pustynia
-	int desert_width = 3 + (rand() % ((width * 2 / 3)));
-	int desert_height = 3 + (rand() % ((height * 2 / 3)));
-	int desert_coordX = rand() % (width - desert_width - 1);
-	int desert_coordY = rand() % (height - desert_height - 1);
-	//std::cout << desert_width << "   " << desert_height << "   " << desert_coordX << "   " << desert_coordY << std::endl;
+
+	//desert associated generation
+	int desert_width = 3 + (rand() % ((width * 2 / 3)));	//width of the desert
+	int desert_height = 3 + (rand() % ((height * 2 / 3)));	//heigth of the desert
+	int desert_coordX = rand() % (width - desert_width - 1);	// X axis (width) index of the beginnig of desert
+	int desert_coordY = rand() % (height - desert_height - 1);	// Y axis (height) index of the beginnig of desert
 	generate_desert(data, desert_width, desert_height, desert_coordX, desert_coordY);
 
-	//jeziora
-	int lake_number = 1 + (rand() % (std::max(width, height) / 20));
+
+	//lake associated generation
+	int lake_number = 1 + (rand() % (std::max(width, height) / 20));	//number of generated lakes
 	while (lake_number > 0) {
-		int lake_radius = 1 + (rand() % (std::min(width, height) / 10));
-		int lake_center_coordX = rand() % width;
-		int lake_center_coordY = rand() % height;
+		int lake_radius = 1 + (rand() % (std::min(width, height) / 10));	//radius of the lake 
+		int lake_center_coordX = rand() % width;		// X axis (width) index of the middle point of lake
+		int lake_center_coordY = rand() % height;	// Y axis (height) index of the middle point of lake
 		generate_lake(data,width,height, lake_center_coordX, lake_center_coordY, lake_radius);
 		lake_number--;
 	}
-/*
-	if (side) {
-		//LEWO
-
-		//potrzebny wiersz
-		for (int j = river_top_start - turn_length - 1; j >= 0; j--) {
-			humidty_value = data[river_turn][j + 1].getHumidity() - (1 + rand() % 4);
-			data[river_turn][j].setHumidity(humidty_value);
-		}
-
-		for (int j = river_top_start + 1; j <= width; j++) {\
-			humidty_value = data[river_turn][j - 1].getHumidity() - (1 + rand() % 4);
-			data[river_turn][j].setHumidity(humidty_value);
-		}
-		
-		//æwiartka NW
-		for (int i = river_turn - 1; i >= 0; i--) {
-			for (int j = river_top_start - 1; j >= 0; j--) {
-				humidty_value = ((data[i + 1][j].getHumidity() + data[i][j + 1].getHumidity()) / 2) - (1 + rand() % 4);
-				data[i][j].setHumidity(humidty_value);
-			}
-		}
-
-
-		//æwiartka NE
-		for (int i = river_turn - 1; i >= 0; i--) {
-			for (int j = river_top_start + 1; j <= width; j++) {
-				humidty_value = ((data[i + 1][j].getHumidity() + data[i][j - 1].getHumidity()) / 2) - (1 + rand() % 4);
-				data[i][j].setHumidity(humidty_value);
-			}
-		}
-
-		//æwiartka SE
-		for (int i = river_turn + 1; i <= height; i++) {
-			for (int j = new_river + 1; j <= width; j++) {
-				humidty_value = ((data[i - 1][j].getHumidity() + data[i][j - 1].getHumidity()) / 2) - (1 + rand() % 4);
-				data[i][j].setHumidity(humidty_value);
-			}
-		}
-
-		//æwiartka SW
-		for (int i = river_turn + 1; i <= height; i++) {
-			for (int j = new_river - 1; j >= 0; j--) {
-				humidty_value = ((data[i - 1][j].getHumidity() + data[i][j + 1].getHumidity()) / 2) - (1 + rand() % 4);
-				data[i][j].setHumidity(humidty_value);
-			}
-		}
-
-	}
-	else {
-		//PRAWO
-
-		//Potrzebny wiersz
-		for (int j = river_top_start + turn_length + 1; j <= width; j++) {
-			humidty_value = data[river_turn][j - 1].getHumidity() - (1 + rand() % 4);
-			data[river_turn][j].setHumidity(humidty_value);
-		}
-		for (int j = river_top_start - 1; j >= 0; j--) {
-			humidty_value = data[river_turn][j + 1].getHumidity() - (1 + rand() % 4);
-			data[river_turn][j].setHumidity(humidty_value);
-		}
-
-		//æwiartka NW
-		for (int i = river_turn - 1 ; i >=0; i--) {
-			for (int j = river_top_start - 1; j >=0; j--) {
-				humidty_value = ((data[i + 1][j].getHumidity() + data[i][j + 1].getHumidity()) / 2) - (1 + rand() % 4);
-				data[i][j].setHumidity(humidty_value);  
-			}
-		}
-
-		//æwiartka NE
-		for (int i = river_turn - 1; i >= 0; i--) {
-			for (int j = river_top_start + 1; j <= width; j++) {
-				humidty_value = ((data[i + 1][j].getHumidity() + data[i][j - 1].getHumidity()) / 2) - (1 + rand() % 4);
-				data[i][j].setHumidity(humidty_value);
-			}
-		}
-
-		//æwiartka SE
-		for (int i = river_turn + 1; i <= height; i++) {
-			for (int j = new_river + 1; j <= width; j++) {
-				humidty_value = ((data[i - 1][j].getHumidity() + data[i][j - 1].getHumidity()) / 2) - (1 + rand() % 4);
-				data[i][j].setHumidity(humidty_value);
-			}
-		}
-
-		//æwiartka SW
-		for (int i = river_turn + 1; i <= height; i++) {
-			for (int j = new_river - 1; j >= 0; j--) {
-				humidty_value = ((data[i - 1][j].getHumidity() + data[i][j + 1].getHumidity()) / 2) - (1 + rand() % 4);
-				data[i][j].setHumidity(humidty_value);
-			}
-		}
-	}
-
-	//TEST
-	*/
 	
+
+	//Debug printing all humidity values
+	/*
 	for (int j = 0; j <= height; j++) {
 		std::cout << "RZ¥D: " << j << std::endl;
 		for (int i = 0; i <= width; i++) {
@@ -254,5 +223,6 @@ void env_humidity::generate_humidity(sprite_params** data, int width, int height
 		}
 		std::cout << std::endl;
 	}
+	*/
 	
 }

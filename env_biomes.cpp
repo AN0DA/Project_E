04cbefaa57d0a02212bb028978640c3658af98f1
Konstanty Kordas <kostekkor@gmail.com>
2020-05-27 22:43:17 +0200

    Biomes class comented

--- env_biomes.cpp
+++ env_biomes.cpp
@@ -1,22 +1,37 @@
+/// \file
+/// \brief Source file for env_biomes
+/// \details Source file for env_biomes environment parameter. Sets the biome of every tile.
+/// \author Konstanty Kordas
+/// \date 27.05.2020
+/// \version 0.1
+
 #include "env_biomes.h"
 
+/// \brief Script setting the biome af every tile
+/// \author Konstanty Kordas
+/// \date 27.05.2020
+/// \version 0.1
+///
+///Biome is generated based on tiles water status, temperature and humidity
+///Full biome parameter table in core.h 
 void env_biomes::generate_biomes(sprite_params** data, int width, int height) {
 	Biomes b;
 	for (int i = 0; i <= height; i++) {
 		for (int j = 0; j <= width; j++) {
-			humidity = data[i][j].getHumidity();
-			temperature = data[i][j].get_temperature();
+			double humidity = data[i][j].getHumidity();
+			double temperature = data[i][j].get_temperature();
 			if (data[i][j].isWater()) {
 				if (temperature >= 0) {
 					data[i][j].setBiome(Biomes::water);
 					//std::cout << "water"<< " ";
-
 				}
 				else {
 					data[i][j].setBiome(Biomes::ice);
 					//std::cout << "ice"<< " ";
 				}
 			}
+
+
 			else if (temperature >= 25) {
 				if (humidity >= 90) {
 					data[i][j].setBiome(Biomes::swamp);
@@ -25,10 +40,6 @@ void env_biomes::generate_biomes(sprite_params** data, int width, int height) {
 				else if (humidity > 75) {
 					data[i][j].setBiome(Biomes::rainforest);
 					//std::cout << "rainforest" << " ";
-				}
-				else if (humidity > 50) {
-					data[i][j].setBiome(Biomes::grassland);
-					//std::cout << "desert" << " ";
 				}
 				else if (humidity < 20) {
 					data[i][j].setBiome(Biomes::desert);
@@ -47,8 +58,10 @@ void env_biomes::generate_biomes(sprite_params** data, int width, int height) {
 				data[i][j].setBiome(Biomes::snow);
 				//std::cout << "snow" << " ";
 			}
+			//Debug printing 
 			//std::cout << data[i][j].getBiome()<<" ";
 		}
+		//Debug printing 
 		//std::cout << std::endl;
 	}
 }
--- env_biomes.h
+++ env_biomes.h
@@ -1,10 +1,21 @@
 #pragma once
-
+/// \headerfile
+/// \brief Header file for env_humidity
+/// \details Header file for env_humidity environment parameter. Generates river, lakes and humidity around them.
+/// \author Konstanty Kordas
+/// \date 27.05.2020
+/// \version 0.1
 #include "Core.h"
 
 class env_biomes {
-	double humidity;
-	double temperature;
+	/// \class env_biomes env_biomes.h
+	/// \brief Class related to setting the biome of all tiles on the grid
+	/// \author Konstanty Kordas
 public:
+	/// Fuction that goes through every tile on the grid and sets its biome based on tiles temperature and humidity
+	///
+	/// \param data sprite_params array passed from main
+	/// \param width sprite_params array width
+	/// \param height sprite_params array height
 	void generate_biomes(sprite_params** data, int width, int height);
 };
